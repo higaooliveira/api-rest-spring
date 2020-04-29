d682258cca05564d37f34b8beb589adc087afb36
Higor <higoroliveiiira@gmail.com>
2020-04-28 21:10:08 -0300

    Create order status enum

--- src/main/java/com/higao/apirestspring/config/TestProfileConfig.java
+++ src/main/java/com/higao/apirestspring/config/TestProfileConfig.java
@@ -2,6 +2,7 @@ package com.higao.apirestspring.config;
 
 import com.higao.apirestspring.entities.Order;
 import com.higao.apirestspring.entities.User;
+import com.higao.apirestspring.entities.enums.OrderStatus;
 import com.higao.apirestspring.repositories.OrderRepository;
 import com.higao.apirestspring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestProfileConfig implements CommandLineRunner {
         User firstUser = new User("Higor Teste 1", "higor@gmail.com", "999999999", "123456");
         User secondUser = new User("Higor Teste 2", "higor@gmail.com", "999999999", "123456");
 
-        Order firstOrder = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), firstUser);
-        Order secondOrder = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), secondUser);
-        Order thirdOrder = new Order(null, Instant.parse("2019-06-20T19:54:07Z"), firstUser);
+        Order firstOrder = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, firstUser);
+        Order secondOrder = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, secondUser);
+        Order thirdOrder = new Order(null, Instant.parse("2019-06-20T19:54:07Z"), OrderStatus.WAITING_PAYMENT, firstUser);
         this.userRepository.saveAll(Arrays.asList(firstUser, secondUser));
         this.orderRepository.saveAll(Arrays.asList(firstOrder, secondOrder, thirdOrder));
     }
--- src/main/java/com/higao/apirestspring/entities/Order.java
+++ src/main/java/com/higao/apirestspring/entities/Order.java
@@ -1,6 +1,7 @@
 package com.higao.apirestspring.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.higao.apirestspring.entities.enums.OrderStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -20,15 +21,19 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant date;
 
+    private int orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private User customer;
 
+
     public Order (){}
 
-    public Order(Long id, Instant date, User customer) {
+    public Order(Long id, Instant date,  OrderStatus orderStatus, User customer) {
         this.id = id;
         this.date = date;
+        this.setOrderStatus(orderStatus);
         this.customer = customer;
     }
 
@@ -54,6 +59,14 @@ public class Order implements Serializable {
 
     public void setCustomer(User customer) {
         this.customer = customer;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(this.orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus.getCode();
     }
 
     @Override
